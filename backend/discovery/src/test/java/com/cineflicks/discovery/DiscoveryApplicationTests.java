96c852f341ed110ea97fb519b6f0242007228a2d
Azza Maddouri <90465414+azzamaddouri@users.noreply.github.com>
2024-10-27 00:16:58 +0200

    Setup project infrastructure !

---
+++ backend/discovery/src/test/java/com/cineflicks/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineflicks.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
